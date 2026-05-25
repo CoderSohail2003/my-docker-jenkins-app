894d2b709435a32cfb98e7f4ddbbeed3baed5b1c
CoderSohail2003 <163769670+CoderSohail2003@users.noreply.github.com>
2026-05-26 01:30:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.write("Hello from Node App");
+    res.write("Hello from Node App, Change 1");
     res.end();
 }).listen(3000);
